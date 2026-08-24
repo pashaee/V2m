4b2428fda50b37c4029fd2a5c9487c717b3fbc6f
pasha <sirous.pasha.k@gmail.com>
2026-08-24 16:53:30 +0330

    Add auto-update URL and Keystore signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 745
-        versionName = (project.findProperty("pattngVersion") as? String)?.takeIf { it.isNotBlank() } ?: "2.3.5"
+        versionName = "1.0.0"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
@@ -89,7 +89,7 @@ android {
                 .map { it as com.android.build.gradle.internal.api.ApkVariantOutputImpl }
                 .forEach { output ->
                     val abi = output.getFilter("ABI") ?: "universal"
-                    output.outputFileName = "V2raymasters_${variant.versionName}_${abi}.apk"
+                    output.outputFileName = "v2m_${variant.versionName}_${abi}.apk"
                     if (versionCodes.containsKey(abi)) {
                         output.versionCodeOverride =
                             (100 * variant.versionCode + versionCodes[abi]!!).plus(5000000)
@@ -109,7 +109,7 @@ android {
                     else
                         "universal"
 
-                    output.outputFileName = "V2raymasters_${variant.versionName}_${abi}.apk"
+                    output.outputFileName = "v2m_${variant.versionName}_${abi}.apk"
                     if (versionCodes.containsKey(abi)) {
                         output.versionCodeOverride =
                             (1000000 * versionCodes[abi]!!).plus(variant.versionCode)
--- app/src/main/java/com/v2ray/ang/AppConfig.kt
+++ app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -128,7 +128,7 @@ object AppConfig {
     const val GITHUB_DOWNLOAD_URL = "$GITHUB_URL/%s/releases/latest/download"
     const val ANDROID_PACKAGE_NAME_LIST_URL = "$GITHUB_RAW_URL/2dust/androidpackagenamelist/master/proxy.txt"
     const val APP_URL = "$GITHUB_URL/pashaee"
-    const val APP_API_URL = "https://api.github.com/repos/pashaee/PattNG/releases"
+    const val APP_API_URL = "https://api.github.com/repos/pashaee/V2m/releases/latest"
     const val APP_ISSUES_URL = "$APP_URL/issues"
     const val APP_WIKI_MODE = "$APP_URL/wiki/Mode"
     const val APP_PRIVACY_POLICY = "$GITHUB_RAW_URL/2dust/v2rayNG/master/CR.md"
