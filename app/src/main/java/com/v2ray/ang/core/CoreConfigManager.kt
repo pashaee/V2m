f5301f2ff5ee37f60b04e8c4bdf7aee206df9b81
pasha <sirous.pasha.k@gmail.com>
2026-08-30 10:52:55 +0330

    Update AndroidLibXrayLite

--- app/src/main/java/com/v2ray/ang/core/CoreConfigManager.kt
+++ app/src/main/java/com/v2ray/ang/core/CoreConfigManager.kt
@@ -192,6 +192,10 @@ object CoreConfigManager {
                       "streamSettings": {
                         "network": "ws",
                         "security": "tls",
+                        "sockopt": {
+                        "tcpNoDelay": true,
+                        "tcpFastOpen": true
+                                    },
                         "tlsSettings": {
                           "serverName": "$dynSni",
                           "alpn": [
@@ -203,7 +207,16 @@ object CoreConfigManager {
                         "wsSettings": {
                           "path": "/",
                           "headers": {
-                            "User-Agent": "edge"
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+                                        "Accept-Language": "fa-IR,fa;q=0.9,en-US;q=0.8,en;q=0.7",
+                                        "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+                                        "sec-ch-ua-mobile": "?0",
+                                        "sec-ch-ua-platform": "\"Windows\"",
+                                        "Sec-Fetch-Dest": "websocket",
+                                        "Sec-Fetch-Mode": "websocket",
+                                        "Sec-Fetch-Site": "same-origin",
+                                        "Cache-Control": "no-cache",
+                                        "Pragma": "no-cache"
                           }
                         }
                       }
